e2af3fdc0c10908535b13a2dd83affb312685722
Kris <Valentina10@sibmail.com>
2022-11-29 22:05:45 +0700

    Adding a diagram from the For_Lab3_partB file.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -68,5 +68,65 @@ MainWindow::MainWindow(QWidget *parent)
     setCentralWidget(splitter); //
 
 
+    //1.Добавление диаграммы
+        QChartView *chartView;
+        QChart *chartBar =  themeWidget->createBarChart(5);
+        chartView = new QChartView(chartBar);
 
+        //splitter->addWidget(themeWidget);
+        //splitter->addWidget(chartView);
+        splitter->addWidget(chartView);
+        setCentralWidget(splitter);
+
+        QItemSelectionModel *selectionModel = treeView->selectionModel();
+        QModelIndex rootIx = dirModel->index(0, 0, QModelIndex());//корневой элемент
+
+        QModelIndex indexHomePath = dirModel->index(homePath);
+        QFileInfo fileInfo = dirModel->fileInfo(indexHomePath);
+
+        /* Рассмотрим способы обхода содержимого папок на диске.
+         * Предлагается вариант решения, которы может быть применен для более сложных задач.
+         * Итак, если требуется выполнить анализ содержимого папки, то необходимо организовать обход содержимого. Обход выполняем относительно модельного индекса.
+         * Например:*/
+        if (fileInfo.isDir()) {
+            /*
+             * Если fileInfo папка то заходим в нее, что бы просмотреть находящиеся в ней файлы.
+             * Если нужно просмотреть все файлы, включая все вложенные папки, то нужно организовать рекурсивный обход.
+            */
+            QDir dir  = fileInfo.dir();
+
+            if (dir.cd(fileInfo.fileName())) {
+                /**
+                 * Если зашли в папку, то пройдемся по контейнеру QFileInfoList ,полученного методом entryInfoList,
+                 * */
+
+                foreach (QFileInfo inf, dir.entryInfoList(QDir::Files | QDir::NoDotAndDotDot, QDir::Type)) {
+                    qDebug() << inf.fileName() << "---" << inf.size();
+                }
+
+                dir.cdUp();//выходим из папки
+            }
+        }
+
+        QDir dir = fileInfo.dir();
+
+        foreach (QFileInfo inf, dir.entryInfoList(QDir::Files | QDir::Dirs | QDir::NoDotAndDotDot, QDir::Type)) {
+
+            qDebug() << inf.fileName() << "---" << inf.size();
+        }
+
+
+        treeView->header()->resizeSection(0, 200);
+        //Выполняем соединения слота и сигнала который вызывается когда осуществляется выбор элемента в TreeView
+        connect(selectionModel, SIGNAL(selectionChanged(const QItemSelection &, const QItemSelection &)),
+                this, SLOT(on_selectionChangedSlot(const QItemSelection &, const QItemSelection &)));
+        //Пример организации установки курсора в TreeView относит ельно модельного индекса
+        QItemSelection toggleSelection;
+        QModelIndex topLeft;
+        topLeft = dirModel->index(homePath);
+        dirModel->setRootPath(homePath);
+
+        toggleSelection.select(topLeft, topLeft);
+        selectionModel->select(toggleSelection, QItemSelectionModel::Toggle);
+    }
 }
