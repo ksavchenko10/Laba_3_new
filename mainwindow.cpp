1a22f248788b02b46bbe4f5a7e3d68a02f41df01
Kris <Valentina10@sibmail.com>
2022-11-29 16:15:45 +0700

    Create MainWindow referring to the code from the For_Lab3_partB file.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -37,3 +37,33 @@
 #include <QtCharts/QBarCategoryAxis>
 
 
+MainWindow::MainWindow(QWidget *parent)
+    : //QWidget(parent)
+      QMainWindow(parent)
+{
+    themeWidget = new ThemeWidget();
+    //Устанавливаем размер главного окна
+    this->setGeometry(100, 100, 1500, 500);
+    this->setStatusBar(new QStatusBar(this));
+    this->statusBar()->showMessage("Choosen Path: ");
+    QString homePath = QDir::homePath();
+    // Определим  файловой системы:
+    dirModel =  new QFileSystemModel(this);
+    dirModel->setFilter(QDir::NoDotAndDotDot | QDir::AllDirs);
+    dirModel->setRootPath(homePath);
+
+    fileModel = new QFileSystemModel(this);
+    fileModel->setFilter(QDir::NoDotAndDotDot | QDir::Files);
+
+    fileModel->setRootPath(homePath);
+    //Показать как дерево, пользуясь готовым видом:
+
+    treeView = new QTreeView();
+    treeView->setModel(dirModel);
+
+    treeView->expandAll();
+    QSplitter *splitter = new QSplitter(parent);
+    tableView = new QTableView;
+    tableView->setModel(fileModel);
+    splitter->addWidget(treeView);
+    //splitter->addWidget(tableView);
